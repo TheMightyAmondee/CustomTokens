4b3e651bd9bbe8c0b6b055fe4957e7fc036f0423
Amelie <amelie.parkinson.bates@gmail.com>
2020-09-30 16:25:03 +1000

    added custom token for player name

--- CustomTokens/ModEntry.cs
+++ CustomTokens/ModEntry.cs
@@ -7,29 +7,52 @@ using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
 using StardewValley.Locations;
+using System.Collections.Generic;
+
 
 namespace CustomTokens
 {
-   public class ModEntry : Mod
+    public interface IContentPatcherAPI
     {
+        void RegisterToken(IManifest mod, string name, Func<IEnumerable<string>> getValue);
+    }
+    public class ModEntry : Mod
+    {
+
+
         public override void Entry(IModHelper helper)
         {
             helper.Events.Player.Warped += this.LocationChange;
-            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+            helper.Events.GameLoop.GameLaunched += this.GameLaunched;
         }
 
-
-        private void OnButtonPressed(object sender, ButtonPressedEventArgs b)
+        private void GameLaunched(object sender, GameLaunchedEventArgs e)
         {
-            this.Monitor.Log($"{Game1.player.Name} pressed {b.Button}.", LogLevel.Debug);
+            this.Monitor.Log($"Game Launched", LogLevel.Debug);
+            var api = this.Helper.ModRegistry.GetApi<IContentPatcherAPI>("Pathoschild.ContentPatcher");
+            api.RegisterToken(this.ModManifest, "PlayerName", () =>
+            {
+                if (Context.IsWorldReady)
+                    return new[] { Game1.player.Name };
+                if (SaveGame.loaded?.player != null)
+                    return new[] { SaveGame.loaded.player.Name }; // lets token be used before save is fully loaded
+                return null;
+            });
+            api.RegisterToken(this.ModManifest, "MineLevel", () =>
+            {
+                if (Context.IsPlayerFree)
+                    return new[] { Game1.CurrentMineLevel.ToString()};
+                if (SaveGame.loaded?.player != null)
+                    return new[] { SaveGame.loaded.player.Name}; // lets token be used before save is fully loaded
+                return null;
+            });
         }
-
-
+        
         private void LocationChange(object sender, WarpedEventArgs e)
         {
-
+            this.Monitor.Log($"{Game1.player.Name} warped from {e.OldLocation} to {e.NewLocation}", LogLevel.Debug);
             var mineShaft = Game1.currentLocation as MineShaft;
-
+           
             if (!(mineShaft is null))
             {
                 this.Monitor.Log($"{Game1.player.Name} is on level {mineShaft.mineLevel}.", LogLevel.Debug);
@@ -37,3 +60,5 @@ namespace CustomTokens
         }
     }
 }
+
+
