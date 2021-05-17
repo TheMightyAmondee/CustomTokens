dd440979955c43925067a82809a61b1c17406346
Amelie <amelie.parkinson.bates@gmail.com>
2021-05-17 12:02:10 +1000

    remove unused directive

--- CustomTokens/QuestData.cs
+++ CustomTokens/QuestData.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using StardewValley.Quests;
 using StardewModdingAPI;
 using StardewModdingAPI.Utilities;
